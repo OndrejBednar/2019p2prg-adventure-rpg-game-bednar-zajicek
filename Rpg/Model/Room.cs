6012d940d7bda6340088fc597a1d833351549774
OndraBednar <ondrej.bednar@pslib.cz>
2020-04-19 03:28:04 +0200

    Room

---
+++ Rpg/Model/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Rpg.Model
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public string Description { get; set; }
+        public List<Crossroad> Crossroad { get; set; }
+    }
+}
