82496c67b9b69764f06b546623d22a51f9783d71
Jan Zajíček <u18.430@pslib.cloud>
2020-04-19 18:53:54 +0200

    commit

--- Rpg/Model/Crossroad.cs
+++ Rpg/Model/Crossroad.cs
@@ -7,6 +7,13 @@ namespace Rpg.Model
 {
     public class Crossroad
     {
+        public Crossroad(int crossroadID, int roomID, int nextRoomID)
+        {
+            CrossroadID = crossroadID;
+            RoomID = roomID;
+            NextRoomID = nextRoomID;
+        }
+
         public int CrossroadID { get; set; }
         public int RoomID { get; set; }
         public int NextRoomID { get; set; }
--- Rpg/Model/Room.cs
+++ Rpg/Model/Room.cs
@@ -9,14 +9,21 @@ namespace Rpg.Model
 {
     public class Room
     {
-        public Room(int id)
+        public Room(int id, int[] crossroads)
         {
             RoomID = id;
             SetDescription();
+            for (int i = 0; i < crossroads.Length; i++)
+            {
+                if(crossroads[i] != -1)
+                {
+                    Crossroads.Add(new Crossroad(i, RoomID, crossroads[i]));
+                }
+            }
         }
         public int RoomID { get; set; }
         public string Description { get; set; }
-        public List<Crossroad> Crossroad { get; set; }
+        public List<Crossroad> Crossroads { get; set; }
             
         private void SetDescription()
         {
