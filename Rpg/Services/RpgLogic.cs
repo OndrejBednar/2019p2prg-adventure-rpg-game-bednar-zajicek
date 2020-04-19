efea2e2801bd7a79bcd7a686012aa16e04799dd0
OndraBednar <ondrej.bednar@pslib.cz>
2020-04-19 17:26:07 +0200

    things

---
+++ Rpg/Services/RpgLogic.cs
@@ -0,0 +1,16 @@
+﻿using Rpg.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rpg.Services
+{
+    public class RpgLogic
+    {
+        public void Play(Room room)
+        {
+
+        }
+    }
+}
