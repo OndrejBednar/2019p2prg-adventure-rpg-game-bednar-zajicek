21ed0458c52c5d62f555f9b961605482197e1048
OndraBednar <ondrej.bednar@pslib.cz>
2020-04-19 16:35:16 +0200

    session init

---
+++ Rpg/Services/SessionStorage.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rpg.Services
+{
+    public class SessionStorage
+    {
+        readonly ISession _session;
+        const string key = "key";
+
+        public SessionStorage(IHttpContextAccessor hce)
+        {
+            _session = hce.HttpContext.Session;
+        }
+
+        private void Save(int data)
+        {
+            _session.SetInt32(key, data);
+        }
+    }
+}
